2f26f6129a843c737bfa04fe450f96aae59300e0
kumarmohit-28 <65715673+kumarmohit-28@users.noreply.github.com>
2020-09-24 01:41:40 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,4 +1,7 @@
+
 USE abc;
-CREATE TABLE users(username VARCHAR(50) NOT NULL PRIMARY KEY, email VARCHAR(50),password VARCHAR(50),name VARCHAR(50),rollno TEXT(50),branch VARCHAR(50),hostelname VARCHAR(100),roomno VARCHAR(50),mobileno TEXT(100));
+CREATE TABLE if not exists users(username VARCHAR(50) NOT NULL PRIMARY KEY, email VARCHAR(50),password VARCHAR(50),name VARCHAR(50),rollno TEXT(50),branch VARCHAR(50),hostelname VARCHAR(100),roomno VARCHAR(50),mobileno TEXT(100));
 
 CREATE TABLE if not exists complaints(complaint_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), subject TEXT,category TEXT,time_of_availability VARCHAR(100),uergency TEXT,details text);
+
+CREATE TABLE if not exists suggetions(suggetion_id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(100), subject TEXT,category TEXT,details text);
