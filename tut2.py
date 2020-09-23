0dfa95898aad9a7bbcc1760cf0e9f0c76867819c
kumarmohit-28 <65715673+kumarmohit-28@users.noreply.github.com>
2020-09-24 01:42:25 +0530

    Add files via upload

--- tut2.py
+++ tut2.py
@@ -31,40 +31,41 @@ def send_mail(reciver,passw):
 #session['hostel']='HALL OF RESIDENCE'
 @app.before_first_request
 def init_app():
-    session['hostelname'] = 'HALL OF RESIDENCE'
-
+    session['hostelname'] = 'WELCOME TO IIT INDORE'
+    session['show'] = True
 
 @app.route('/')
 def home():
+    session['hostelname'] = 'WELCOME TO IIT INDORE'
     return render_template('home.html')
     #return redirect(url_for('login'))
 @app.route('/apj')
 def apj():
-    session['hostelname']='A. P. J ABDUL KALAM HALL OF RESIDENCE'
-    return redirect(url_for('home'))
+    session['hostelname']='A. P. J ABDUL KALAM'
+    return render_template('apj.html')
 @app.route('/vsb')
 def vsb():
-    session['hostelname']='VIKRAM SARABHAI HALL OF RESIDENCE'
+    session['hostelname']='VIKRAM SARABHAI'
     return redirect(url_for('home'))
 
 @app.route('/cvr')
 def cvr():
-    session['hostelname']='C. V. RAMAN HALL OF RESIDENCE'
+    session['hostelname']='C. V. RAMAN'
     return redirect(url_for('home'))
 
 @app.route('/devi')
 def devi():
-    session['hostelname']='DEVI AHILYA HALL OF RESIDENCE'
+    session['hostelname']='DEVI AHILYA'
     return redirect(url_for('home'))
 
 @app.route('/homi')
 def homi():
-    session['hostelname']='HOMI JEHANGIR BHABHA HALL OF RESIDENCE'
+    session['hostelname']='HOMI JEHANGIR BHABHA'
     return redirect(url_for('home'))
 
 @app.route('/jcb')
 def jcb():
-    session['hostelname']='J. C. BOSE HALL OF RESIDENCE'
+    session['hostelname']='J. C. BOSE'
     return redirect(url_for('home'))
 
 @app.route('/login',methods=['GET','POST'])
@@ -80,7 +81,7 @@ def login():
         if (x!=0):
             data=cur1.fetchone()
             #print(data)
-            if(data[3]==password):
+            if(data[2]==password):
                 session['logged_in']=True
                 return redirect(url_for('home'))
             else:
@@ -96,6 +97,26 @@ def login():
 def logout():
     session['logged_in']=False
     return redirect(url_for('home'))
+@app.route('/userdetails',methods=["GET","POST"])
+def userdetails():
+    if(request.method=='POST'):
+        name=request.form.get('fullname')
+        rollno=request.form.get('rollno')
+        branch=request.form.get('branch')
+        hostelname=request.form.get('hostelname')
+        roomno=request.form.get('roomno')
+        mobileno=request.form.get('mobileno')
+        if(len(name)==0 or len(rollno)==0 or len(branch)==0 or len(hostelname)==0 or len(roomno)==0 or len(mobileno)==0):
+            flash('enter a valid details')
+            return redirect(url_for('userdetails'))
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(username,email,password,name,rollno,branch,hostelname,roomno,mobileno) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (session['username'],session['email'], session['password'],name,rollno,branch,hostelname,roomno,mobileno))
+        mysql.connection.commit()
+        cur.close()
+        session['logged_in'] = True
+        return redirect(url_for('home'))
+    return render_template('userdetails.html')
+
 
 @app.route('/signup',methods=["GET","POST"])
 def signup():
@@ -104,11 +125,13 @@ def signup():
         username=request.form.get('username')
         email=request.form.get('email')
         password=request.form.get('password')
+        if(email[-10:]!='iiti.ac.in'):
+            flash('Please use institute mail id only')
+            return redirect(url_for('signup'))
         if(len(username)==0 or len(email)==0 or len(password)==0):
             flash('enter a valid details')
             return redirect(url_for('signup'))
         cur=mysql.connection.cursor()
-        #print(username,password,email)
         x=cur.execute("SELECT * FROM users WHERE username=(%s)",(username,))
         if(int(x)>0):
             flash("USERNAME ALREADY EXIST PLEASE USE ANOTHER")
@@ -117,27 +140,70 @@ def signup():
         if (int(x) > 0):
             flash("USER ALREADY REGISTERED WITH THIS EMAIL ID")
             return render_template('signup.html')
-        cur.execute("INSERT INTO users(username,email,password) VALUES(%s,%s,%s)",(username,email,password))
-        mysql.connection.commit()
         cur.close()
-        flash('successfully registered please login')
-        return redirect(url_for('login'))
+        flash('successfully registered please enter your details')
+        session['username']=username
+        session['email']=email
+        session['password']=password
+        return redirect(url_for('userdetails'))
     return render_template('signup.html')
 
 @app.route('/forgot-password',methods=['GET','POST'])
 def forgotpassword():
     if(request.method=='POST'):
         email = request.form.get('email')
-        #print(email)
         cur = mysql.connection.cursor()
-        # print(username,password,email)
         x = cur.execute("SELECT * FROM users WHERE email=(%s)", (email,))
         if (int(x) > 0):
             data = cur.fetchone()
-            send_mail(email, data[3])
+            send_mail(email, data[2])
             return redirect(url_for('login'))
         else:
             flash('Email not registered')
             return render_template('forgotpassword.html')
     return render_template('forgotpassword.html')
+@app.route('/complaints',methods=['GET','POST'])
+def complaints():
+    if (session['logged_in'] == False):
+        flash('PLEASE LOG IN FIRST')
+        return redirect(url_for('login'))
+    session['hostelname']='REGISTER YOUR COMPLAINTS'
+    session['show']=False
+    if(request.method=='POST'):
+        subject=request.form.get('subject')
+        category=request.form.get('category')
+        timeofavilibility=request.form.get('timeofavial')
+        urgency=request.form.get('urgency')
+        details=request.form.get('details')
+        cur=mysql.connection.cursor()
+        cur.execute("INSERT INTO complaints(username,subject,category,time_of_availability,uergency,details) VALUES(%s,%s,%s,%s,%s,%s)",(session['username'],subject,category,timeofavilibility,urgency,details))
+        mysql.connection.commit()
+        cur.close()
+        flash('Complaint registered')
+        return render_template('complaints.html',scrollToAnchor='contact')
+
+    return render_template('complaints.html')
+
+@app.route('/suggetions',methods=['GET','POST'])
+def suggetions():
+    if(session['logged_in']==False):
+        flash('PLEASE LOG IN FIRST')
+        return redirect(url_for('login'))
+    session['hostelname']='PROVIDE YOUR VALUEABLE SUGGETIONS'
+    session['show']=False
+    if(request.method=='POST'):
+        subject=request.form.get('subject')
+        category=request.form.get('category')
+        details=request.form.get('details')
+        cur=mysql.connection.cursor()
+        cur.execute("INSERT INTO suggetions(username,subject,category,details) VALUES(%s,%s,%s,%s)",(session['username'],subject,category,details))
+        mysql.connection.commit()
+        cur.close()
+        flash('THANK YOU FOR YOUR VALUEABLE SUGGETION WE WILL IMPORVE OURSELEVES')
+        return render_template('suggetions.html',scrollToAnchor='contact')
+
+    return render_template('suggetions.html')
+
+
+
 app.run(debug=True)
