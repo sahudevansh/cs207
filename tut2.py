09e7a0da339f6fafcef69881ed2e54000378575c
kumarmohit-28 <65715673+kumarmohit-28@users.noreply.github.com>
2020-10-12 17:47:30 +0530

    Add files via upload

--- tut2.py
+++ tut2.py
@@ -1,8 +1,9 @@
 from flask import Flask,render_template,request,flash,redirect,url_for,session
 from flask_mysqldb import MySQL
 from flask_mail import Mail,Message
+import random
 app=Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'abc'
@@ -22,19 +23,31 @@ app.config.update(
 mail=Mail(app)
 def send_mail(reciver,passw):
     msg=Message('Password',
-                sender='allen.kumarmohit@gmail.com',
+                sender='cs207hostelproject@gmail.com',
                 recipients=[reciver])
     msg.body="Here is your password :"+str(passw)
     mail.send(msg)
     flash('password sent succesfully please log in')
     return
-#session['hostel']='HALL OF RESIDENCE'
+
+def send_otp(reciver,otp):
+    msg=Message('OTP',
+                sender='cs207hostelproject@gmail.com',
+                recipients=[reciver])
+    msg.body="Here is your one time password :"+str(otp)
+    mail.send(msg)
+    flash('otp sent succesfully please validate')
+    return
+
+
+
 @app.before_first_request
 def init_app():
     session['hostelname'] = 'WELCOME TO IIT INDORE'
     session['show'] = True
     session['logged_in'] =False
-
+    session['signup']=False
+    session['otpverify'] = False
 @app.route('/')
 def home():
     session['show'] = True
@@ -49,22 +62,22 @@ def apj():
 @app.route('/vsb')
 def vsb():
     session['hostelname']='VIKRAM SARABHAI'
-    return redirect(url_for('home'))
+    return render_template('vsb.html')
 
 @app.route('/cvr')
 def cvr():
     session['hostelname']='C. V. RAMAN'
-    return redirect(url_for('home'))
+    return render_template('cvr.html')
 
 @app.route('/devi')
 def devi():
     session['hostelname']='DEVI AHILYA'
-    return redirect(url_for('home'))
+    return render_template('devi.html')
 
 @app.route('/homi')
 def homi():
     session['hostelname']='HOMI JEHANGIR BHABHA'
-    return redirect(url_for('home'))
+    return render_template('homi.html')
 
 @app.route('/jcb')
 def jcb():
@@ -73,6 +86,8 @@ def jcb():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if('logged_in' in session and session['logged_in']==True):
+        return redirect(url_for('home'))
     if(request.method=='POST'):
         username = request.form.get('username')
         password = request.form.get('password')
@@ -85,6 +100,7 @@ def login():
             data=cur1.fetchone()
             #print(data)
             if(data[2]==password):
+                session['username'] = data[0]
                 session['logged_in']=True
                 return redirect(url_for('home'))
             else:
@@ -99,9 +115,19 @@ def login():
 @app.route('/logout')
 def logout():
     session['logged_in']=False
+    session['otpverify'] = False
+    session['signup'] = False
     return redirect(url_for('home'))
 @app.route('/userdetails',methods=["GET","POST"])
 def userdetails():
+    if('logged_in' in session and session['logged_in']==True):
+        return redirect(url_for('home'))
+    if ('signup' in session and session['signup'] == False):
+        flash('PLEASE SIGNUP FIRST TO ENTER DETAILS')
+        return redirect('signup')
+    if ('otp' in session and session['otp'] == False):
+        flash('please veryfiy the otp first')
+        return redirect(url_for('otp'))
     if(request.method=='POST'):
         name=request.form.get('fullname')
         rollno=request.form.get('rollno')
@@ -121,16 +147,39 @@ def userdetails():
     return render_template('userdetails.html')
 
 
+@app.route('/otpverification',methods=['GET','POST'])
+def otp():
+    if('logged_in' in session and session['logged_in']==True):
+        return redirect(url_for('home'))
+    if ('signup' in session and session['signup'] == False):
+        flash('PLEASE SIGNUP FIRST TO ENTER DETAILS')
+        return redirect('signup')
+    if(request.method=='POST'):
+        num=request.form.get('otp')
+        #print(num,session['otp'])
+        if(int(num)==session['otp']):
+            session['otpverify']=True
+            session['signup'] = True
+            flash('Successfully registered please enter your details')
+            return redirect(url_for('userdetails'))
+        else:
+            flash('OTP ENTERED IS INCORRECT')
+            return render_template('otpverify.html')
+    return render_template('otpverify.html')
+
+
 @app.route('/signup',methods=["GET","POST"])
 def signup():
+    if('logged_in' in session and session['logged_in']==True):
+        return redirect(url_for('home'))
     if(request.method=='POST'):
         #add to data base
         username=request.form.get('username')
         email=request.form.get('email')
         password=request.form.get('password')
-        if(email[-10:]!='iiti.ac.in'):
+        '''if(email[-10:]!='iiti.ac.in'):
             flash('Please use institute mail id only')
-            return redirect(url_for('signup'))
+            return redirect(url_for('signup'))'''
         if(len(username)==0 or len(email)==0 or len(password)==0):
             flash('enter a valid details')
             return redirect(url_for('signup'))
@@ -144,15 +193,22 @@ def signup():
             flash("USER ALREADY REGISTERED WITH THIS EMAIL ID")
             return render_template('signup.html')
         cur.close()
-        flash('successfully registered please enter your details')
-        session['username']=username
-        session['email']=email
-        session['password']=password
-        return redirect(url_for('userdetails'))
+        x1=random.randrange(111111,999999)
+        send_otp(email,x1)
+        #d={'x':x1,'username':username,'email':email,'password':password}
+        session['username'] = username
+        session['email'] = email
+        session['password'] = password
+        session['otp']=x1
+        session['otpverify'] = False
+        session['signup'] = True
+        return redirect(url_for('otp'))
     return render_template('signup.html')
 
 @app.route('/forgot-password',methods=['GET','POST'])
 def forgotpassword():
+    if('logged_in' in session and session['logged_in']==True):
+        return redirect(url_for('home'))
     if(request.method=='POST'):
         email = request.form.get('email')
         cur = mysql.connection.cursor()
@@ -168,7 +224,7 @@ def forgotpassword():
 @app.route('/complaints',methods=['GET','POST'])
 def complaints():
 
-    if (session['logged_in'] == False):
+    if('logged_in' in session and session['logged_in']==False):
         flash('PLEASE LOG IN FIRST')
         return redirect(url_for('login'))
     session['hostelname']='REGISTER YOUR COMPLAINTS'
@@ -190,7 +246,7 @@ def complaints():
 
 @app.route('/suggetions',methods=['GET','POST'])
 def suggetions():
-    if(session['logged_in']==False):
+    if('logged_in' in session and session['logged_in']==False):
         flash('PLEASE LOG IN FIRST')
         return redirect(url_for('login'))
     session['hostelname']='PROVIDE YOUR VALUEABLE SUGGETIONS'
@@ -207,7 +263,6 @@ def suggetions():
         return render_template('suggetions.html',scrollToAnchor='contact')
 
     return render_template('suggetions.html')
-
 
 
 app.run(debug=True)
